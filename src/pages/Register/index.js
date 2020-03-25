86c2a725e2aa944bf5e50bbc58f66c3a1bc7fd16
SamuelJacome <samuelvictor@rede.ulbra.br>
2020-03-25 13:51:47 -0300

    Page Register implementada

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -15,13 +15,25 @@ export default function Register() {
 
                 <h1> Cadastro </h1>
                 <p>Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem os caso da sua ONG.</p>
-                <Link className = "back-link" to="/register">
+                <Link className = "back-link" to="/">
                     <FiArrowLeft size={16} color="#E02041"/>
-                    Não tenho cadastro</Link>
+                    Já tenho cadastro</Link>
               </section>
               <form>
                   <input placeholder='Nome da ONG'/>
-                  <input type="email"/>
+                  <input type="email" placeholder="E-mail"/>
+                  <input placeholder="WhatsApp"/>
+
+                  <div className="input-group">
+                  
+                      <input placeholder="Cidade"/>
+                   
+                   
+                      <input placeholder="UF"  style={{ width: 80 }} /> 
+                   
+                  </div>
+
+                  <button className = "button" type="submit">Cadastrar</button>
               </form>
           </div>
       </div>
