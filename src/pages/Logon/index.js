4daafff46298c7cc498f688cec41cb5838abb2d9
SamuelJacome <samuelvictor@rede.ulbra.br>
2020-03-25 13:20:13 -0300

    att

--- src/pages/Logon/index.js
+++ src/pages/Logon/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FiLogIn } from 'react-icons/fi'
-
+import { Link }  from 'react-router-dom'
 import './styles.css'
 import logoImg from '../../assets/logo.svg'
 import heroesImg from '../../assets/heroes.png'
@@ -16,9 +16,9 @@ export default function Logon() {
 
                 <button type="submit"  className="button"> Entrar </button>
 
-                <a href="/register">
+                <Link className = "back-link" to="/register">
                     <FiLogIn size={16} color="#E02041"/>
-                    Não tenho cadastro</a>
+                    Não tenho cadastro</Link>
             </form>
 
         </section>
